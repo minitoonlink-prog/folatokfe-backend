b978e6800122c3176e27ceb2fa4aaa04e8e53f64
diegoamcamargo28-netizen <diegoamcamargo28@gmail.com>
2026-06-22 15:26:35 -0500

    Update ProductoController.java

--- folatokfe-backend/src/main/java/com/mycompany/folatokfe/backend/controller/ProductoController.java
+++ folatokfe-backend/src/main/java/com/mycompany/folatokfe/backend/controller/ProductoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/productos")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "${cors.allowed-origins}")
+//@CrossOrigin(origins = "${cors.allowed-origins}")
 public class ProductoController {
 
     private final ProductoService productoService;
